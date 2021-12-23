4b1eb3393dd62591a58a95fe5ba1935710d54af1
skittels15 <50242333+skittels15@users.noreply.github.com>
2021-12-24 00:47:01 +0300

    Added config for BaikalOS

--- config.sh
+++ config.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
-export GITHUB_USER=""
-export GITHUB_EMAIL=""
+export GITHUB_USER="skittels15"
+export GITHUB_EMAIL="skittels1510@mail.ru"
 
-export device=""
+export device="olives"
 
-export ROM=""
-export ROM_DIR=""
-export ROM_VERSION=""
+export ROM="BaikalOS"
+export ROM_DIR="${WORKSPACE}/../baikal"
+export ROM_VERSION="11.1"
 export official="false"
-export local_manifest_url=""
-export manifest_url=""
-export rom_vendor_name=""
-export branch=""
+export local_manifest_url="https://raw.githubusercontent.com/skittels15/manifests/master/baikal-olives.xml"
+export manifest_url="https://github.com/baikalos/manifest"
+export rom_vendor_name="baikalos"
+export branch="r11.1"
 export bacon="bacon"
-export buildtype=""
-export clean=""
-export generate_incremental=""
-export upload_recovery=""
+export buildtype="user"
+export clean="installclean"
+export generate_incremental="false"
+export upload_recovery="false"
 
-export ccache=""
-export ccache_size=""
+export ccache="true"
+export ccache_size="250"
 
-export jenkins="false"
+export jenkins="true"
 
-export release_repo=""
+export release_repo="skittels15/releases"
 
 export timezone="UTC"
