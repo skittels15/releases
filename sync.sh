da7938e606d32a3c0b9cb12a0f4c3311550876f9
skittels15 <50242333+skittels15@users.noreply.github.com>
2022-01-18 00:18:10 +0300

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo "Sync started for ${manifest_url}/tree/${branch}"
 if [ "${jenkins}" == "true" ]; then
-    telegram -M "Sync started for [${ROM} ${ROM_VERSION}](${manifest_url}/tree/${branch}): [See Progress](${BUILD_URL}console)"
+    telegram -M "Синхронизация [${ROM} ${ROM_VERSION}](${manifest_url}/tree/${branch}) начата: [Подробнее](${BUILD_URL}console)"
 else
-    telegram -M "Sync started for [${ROM} ${ROM_VERSION}](${manifest_url}/tree/${branch})"
+    telegram -M "Синхронизация [${ROM} ${ROM_VERSION}](${manifest_url}/tree/${branch}) начата"
 fi
 SYNC_START=$(date +"%s")
 if [ ! -d "${ROM_DIR}"/.repo ]; then
@@ -24,11 +24,11 @@ SYNC_END=$(date +"%s")
 SYNC_DIFF=$((SYNC_END - SYNC_START))
 if [ "${syncsuccessful}" == "0" ]; then
     echo "Sync completed successfully in $((SYNC_DIFF / 60)) minute(s) and $((SYNC_DIFF % 60)) seconds"
-    telegram -N -M "Sync completed successfully in $((SYNC_DIFF / 60)) minute(s) and $((SYNC_DIFF % 60)) seconds"
+    telegram -N -M "Синхронизация завершена успешно через $((SYNC_DIFF / 60)) минут(ы) $((SYNC_DIFF % 60)) секунд(ы)"
     source "${my_dir}/build.sh"
 else
     echo "Sync failed in $((SYNC_DIFF / 60)) minute(s) and $((SYNC_DIFF % 60)) seconds"
-    telegram -N -M "Sync failed in $((SYNC_DIFF / 60)) minute(s) and $((SYNC_DIFF % 60)) seconds"
+    telegram -N -M "Синхронизация завершена с ошибкой через $((SYNC_DIFF / 60)) минут(ы) $((SYNC_DIFF % 60)) секунд(ы)"
     curl --data parse_mode=HTML --data chat_id=$TELEGRAM_CHAT --data sticker=CAADBQADGgEAAixuhBPbSa3YLUZ8DBYE --request POST https://api.telegram.org/bot$TELEGRAM_TOKEN/sendSticker
     exit 1
 fi
