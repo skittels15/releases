4d9ead7dd06e279d98670ecafa8e61c5186b074a
skittels15 <50242333+skittels15@users.noreply.github.com>
2022-01-26 18:41:24 +0300

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -19,6 +19,7 @@ if [ "${cores}" -gt "12" ]; then
     cores=12
 fi
 repo sync
+repo sync --force-sync packages/resources/devicesettings
 syncsuccessful="${?}"
 SYNC_END=$(date +"%s")
 SYNC_DIFF=$((SYNC_END - SYNC_START))
